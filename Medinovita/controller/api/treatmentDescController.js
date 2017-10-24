3e906a667e29ff70c91af27e37ba3d8e8c17f29d
Anjali R <anjeng1989@gmail.com>
2017-10-23 21:04:36 -0400

    treatment fix

--- Medinovita/controller/api/treatmentDescController.js
+++ Medinovita/controller/api/treatmentDescController.js
@@ -153,7 +153,7 @@ module.exports.getTreatmentSection = function (req, res) {
     treatmentDescModel.aggregate([
         {
             "$match": {
-                "$and": [{ "serviceActiveFlag": "Y" }, { "treatmentList.department": req.query.department }, { "treatmentList.activeFlag": "Y" }] }},         
+                "$and": [{ "serviceActiveFlag": "Y" }, { "department": req.query.department }, { "treatmentList.activeFlag": "Y" }] }},         
         {
             "$project": {
                 "_id": 0, "department": 1, "departmentDescription": 1, "departmentImagepath": 1, "treatmentList.procedureName": 1, "treatmentList.displayName": 1, "treatmentList.treatmentDescription": 1, "treatmentList.shortDescription": 1,
